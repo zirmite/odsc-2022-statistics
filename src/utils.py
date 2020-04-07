bd5412bad1db2b40388ac2bc13a40b25a48eb110
Andrew Zirm <andrewzirm@gmail.com>
2020-04-06 21:21:17 -0400

    utils and update notebook

---
+++ src/utils.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+
+def counts_data(mean=100, size=1000, seed=None):
+    if seed is not None:
+        np.random.seed(seed)
+    
+    # generate counts data
+    samples = np.random.poisson(mean, size=size)
+    
+    return pd.Series(data=samples)
