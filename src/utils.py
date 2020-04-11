5d24c041443051d7ab831a85b31471fe68ce9b93
Andrew Zirm <andrewzirm@gmail.com>
2020-04-11 14:21:00 -0400

    start lesson 2 notebook

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import numpy as np
+import scipy.stats as stats
+import seaborn as sns
+
+## Lesson 1 data functions
 
 
 def counts_data(mean=100, size=1000, seed=None):
@@ -12,9 +16,45 @@ def counts_data(mean=100, size=1000, seed=None):
     return pd.Series(data=samples)
 
 
-def counts_data_nan(mean=100, size=1000, seed=None):
-    samples = counts_data(mean, size, seed)
-    samples = samples.astype(np.float64)
-    samples.ravel()[np.random.choice(samples.size, 100, replace=False)] = np.nan
+def add_nan(data, frac=0.05, seed=None):
+    if seed is not None:
+        np.random.seed(seed)
+
+    data.ravel()[
+        np.random.choice(
+            data.size, np.int(data.size * frac * np.random.random()), replace=False
+        )
+    ] = np.nan
+
+    return pd.Series(data=data)
+
+
+def plot_anscombe():
+    # Load the example dataset for Anscombe's quartet
+    df = sns.load_dataset("anscombe")
+
+    # Show the results of a linear regression within each dataset
+    sns.lmplot(
+        x="x",
+        y="y",
+        col="dataset",
+        hue="dataset",
+        data=df,
+        col_wrap=2,
+        ci=None,
+        palette="muted",
+        height=4,
+        scatter_kws={"s": 50, "alpha": 1},
+    )
+
+
+## Lesson 2 data functions
+
+
+def continuous_data(mean=100, std=25.0, size=1000, seed=None):
+    if seed is not None:
+        np.random.seed(seed)
+
+    samples = np.random.normal(loc=mean, scale=std, size=size)
 
     return pd.Series(data=samples)
