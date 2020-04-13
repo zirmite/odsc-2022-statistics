83f9e2059c1e857e19f29454c19b7be15eed4498
Andrew W. Zirm <andrewzirm@gmail.com>
2020-04-13 12:16:35 -0400

    Zirmite/lesson 2 (#2)
    * start lesson 2 notebook
    
    * add some docstrings
    
    * some words
    
    * update requirements
    
    * add editorconfig
    
    * added lesson 3 notebook
    
    * more words

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,14 @@
+"""blah."""
 import pandas as pd
 import numpy as np
+import scipy.stats as stats
+import seaborn as sns
+
+# Lesson 1 data functions
 
 
-def counts_data(mean=100, size=1000, seed=None):
+def tweets_data(mean=50, size=1000, seed=None):
+    """Generate simple column of counts data as a Pandas Series."""
     if seed is not None:
         np.random.seed(seed)
 
@@ -12,9 +18,56 @@ def counts_data(mean=100, size=1000, seed=None):
     return pd.Series(data=samples)
 
 
-def counts_data_nan(mean=100, size=1000, seed=None):
-    samples = counts_data(mean, size, seed)
-    samples = samples.astype(np.float64)
-    samples.ravel()[np.random.choice(samples.size, 100, replace=False)] = np.nan
+def add_nan(data, frac=0.05, seed=None):
+    """
+    Given a column of data, return that column with (at most) the specified
+    fraction of NaN values.
+    """
+    if seed is not None:
+        np.random.seed(seed)
+
+    data.ravel()[
+        np.random.choice(
+            data.size,
+            np.int(data.size * frac * np.random.random()),
+            replace=False,
+        )
+    ] = np.nan
+
+    return pd.Series(data=data)
+
+
+def add_noise(data, sn=5.0):
+    data = data + np.random.normal(loc=0, scale=data.mean()/sn, size=data.size)
+    return data
+
+
+def plot_anscombe():
+    # Load the example dataset for Anscombe's quartet
+    df = sns.load_dataset("anscombe")
+
+    # Show the results of a linear regression within each dataset
+    sns.lmplot(
+        x="x",
+        y="y",
+        col="dataset",
+        hue="dataset",
+        data=df,
+        col_wrap=2,
+        ci=None,
+        palette="muted",
+        height=4,
+        scatter_kws={"s": 50, "alpha": 1},
+    )
+
+
+# Lesson 2 data functions
+
+
+def continuous_data_one(mean=100, std=25.0, size=1000, seed=None):
+    if seed is not None:
+        np.random.seed(seed)
+
+    samples = np.random.normal(loc=mean, scale=std, size=size)
 
     return pd.Series(data=samples)
