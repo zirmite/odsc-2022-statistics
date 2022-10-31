353aba9accb4f1a5ddea05abaf52a853dfdb8130
Andrew Zirm <andrewzirm@gmail.com>
2022-10-31 10:04:28 -0400

    update a function

--- src/utils.py
+++ src/utils.py
@@ -95,3 +95,21 @@ def cumulative_d(data):
     y = np.arange(1, n + 1) / n
 
     return (sorted_data, y)
+
+
+def counts_data(mean=100, size=1000, seed=None):
+    if seed is not None:
+        np.random.seed(seed)
+
+    # generate counts data
+    samples = np.random.poisson(mean, size=size)
+
+    return pd.Series(data=samples)
+
+
+def counts_data_nan(mean=100, size=1000, seed=None):
+    samples = counts_data(mean, size, seed)
+    samples = samples.astype(np.float64)
+    samples.ravel()[np.random.choice(samples.size, 100, replace=False)] = np.nan
+
+    return pd.Series(data=samples)
